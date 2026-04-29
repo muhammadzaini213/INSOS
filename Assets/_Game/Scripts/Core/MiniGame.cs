7019078a34ab4c7b63c1b67f4f908b81e299a14d
muhammadzaini213 <oniichanterritory@gmail.com>
2026-04-29 09:20:50 +0800

    add: setup for base objects

---
+++ Assets/_Game/Scripts/Core/MiniGame.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+
+public abstract class MiniGame : MonoBehaviour
+{
+    [SerializeField] private int starCount; // Number of stars earned in the mini-game
+    private void StartMiniGame()
+    {
+        StartCountdown();
+        // Implement mini-game start logic here
+    }
+
+    private void EndMiniGame()
+    {
+        StopCountdown();
+        // Implement mini-game end logic here
+    }
+
+    private void ResetMiniGame()
+    {
+        // Implement mini-game reset logic here
+    }
+
+    private void StartCountdown()
+    {
+        // Implement countdown logic here
+    }
+
+    private void StopCountdown()
+    {
+        // Implement countdown stop logic here
+    }
+
+
+    private void CountStars()
+    {
+        // Implement star counting logic here
+    }
+
+    public void NotifyPlayerWin()
+    {
+        OnPlayerWin();
+    }
+
+    public void NotifyPlayerLose()
+    {
+        OnPlayerLose();
+    }
+
+    protected void OnPlayerWin()
+    {
+        CountStars();
+        EndMiniGame();
+    }
+
+    protected void OnPlayerLose()
+    {
+        CountStars();
+        EndMiniGame();
+    }
+
+}
