7fdfbd24829f347bff9c31e30bef0d7f7429e686
Muhammad Zaini <163093916+muhammadzaini213@users.noreply.github.com>
2026-09-17 08:14:36 +0800

    feat: memambahkan implementasi audio ke section 1 part 1-8

--- Assets/_Game/00_Scripts/Utils/UI/UIPop.cs
+++ Assets/_Game/00_Scripts/Utils/UI/UIPop.cs
@@ -11,26 +11,45 @@ namespace Slafurry.Utils.UI
     public class UIPop : MonoBehaviour
     {
         [Header("Target")]
-        [SerializeField] private RectTransform target;
+        [SerializeField]
+        private RectTransform target;
 
         [Header("Pop In")]
-        [SerializeField] private float popInDuration = 0.25f;
-        [SerializeField] private float popInStartScale = 0f;
-        [SerializeField] private float popInTargetScale = 1f;
+        [SerializeField]
+        private float popInDuration = 0.25f;
+
+        [SerializeField]
+        private float popInStartScale = 0f;
+
+        [SerializeField]
+        private float popInTargetScale = 1f;
 
         [Header("Pop Out")]
-        [SerializeField] private float popOutDuration = 0.2f;
-        [SerializeField] private float popOutStartScale = 1f;
-        [SerializeField] private float popOutTargetScale = 0f;
+        [SerializeField]
+        private float popOutDuration = 0.2f;
+
+        [SerializeField]
+        private float popOutStartScale = 1f;
+
+        [SerializeField]
+        private float popOutTargetScale = 0f;
 
         [Header("Options")]
-        [SerializeField] private bool useUnscaledTime = true;
+        [SerializeField]
+        private bool useUnscaledTime = true;
+
+        [SerializeField]
+        private bool playOnEnable = false;
 
         [Header("Events")]
-        [SerializeField] private UnityEvent onPopInComplete;
-        [SerializeField] private UnityEvent onPopOutComplete;
+        [SerializeField]
+        private UnityEvent onPopInComplete;
+
+        [SerializeField]
+        private UnityEvent onPopOutComplete;
 
         private Coroutine _routine;
+        private bool _pendingPlay;
 
         private void Awake()
         {
@@ -38,24 +57,34 @@ namespace Slafurry.Utils.UI
                 target = transform as RectTransform;
         }
 
+        private void OnEnable()
+        {
+            if (playOnEnable)
+                _pendingPlay = true;
+        }
+
+        private void LateUpdate()
+        {
+            if (_pendingPlay)
+            {
+                _pendingPlay = false;
+                PopIn();
+            }
+        }
+
+        private void OnDisable()
+        {
+            _pendingPlay = false;
+        }
+
         public void PopIn()
         {
-            StartPop(
-                popInStartScale,
-                popInTargetScale,
-                popInDuration,
-                onPopInComplete
-            );
+            StartPop(popInStartScale, popInTargetScale, popInDuration, onPopInComplete);
         }
 
         public void PopOut()
         {
-            StartPop(
-                popOutStartScale,
-                popOutTargetScale,
-                popOutDuration,
-                onPopOutComplete
-            );
+            StartPop(popOutStartScale, popOutTargetScale, popOutDuration, onPopOutComplete);
         }
 
         public void SetScale(float scale)
@@ -67,37 +96,23 @@ namespace Slafurry.Utils.UI
             target.localScale = Vector3.one * scale;
         }
 
-        private void StartPop(
-            float from,
-            float to,
-            float duration,
-            UnityEvent onComplete)
+        private void StartPop(float from, float to, float duration, UnityEvent onComplete)
         {
             if (_routine != null)
                 StopCoroutine(_routine);
 
-            _routine = StartCoroutine(
-                PopRoutine(from, to, duration, onComplete)
-            );
+            _routine = StartCoroutine(PopRoutine(from, to, duration, onComplete));
         }
 
-        private IEnumerator PopRoutine(
-            float from,
-            float to,
-            float duration,
-            UnityEvent onComplete)
+        private IEnumerator PopRoutine(float from, float to, float duration, UnityEvent onComplete)
         {
             float elapsed = 0f;
 
             while (elapsed < duration)
             {
-                elapsed += useUnscaledTime
-                    ? Time.unscaledDeltaTime
-                    : Time.deltaTime;
+                elapsed += useUnscaledTime ? Time.unscaledDeltaTime : Time.deltaTime;
 
-                float progress = duration > 0f
-                    ? Mathf.Clamp01(elapsed / duration)
-                    : 1f;
+                float progress = duration > 0f ? Mathf.Clamp01(elapsed / duration) : 1f;
 
                 // Ease Out Back
                 float eased = EaseOutBack(progress);
@@ -121,9 +136,7 @@ namespace Slafurry.Utils.UI
             const float c1 = 1.70158f;
             const float c3 = c1 + 1f;
 
-            return 1f +
-                   c3 * Mathf.Pow(t - 1f, 3f) +
-                   c1 * Mathf.Pow(t - 1f, 2f);
+            return 1f + c3 * Mathf.Pow(t - 1f, 3f) + c1 * Mathf.Pow(t - 1f, 2f);
         }
     }
 }
