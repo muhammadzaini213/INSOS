76e2553470c735cc7f4598ea3529db5729afd358
Muhammad Zaini <163093916+muhammadzaini213@users.noreply.github.com>
2026-09-18 20:24:42 +0800

    Revert "fix: poll TouchScreenKeyboard text into TMP_InputField"
    This reverts commit 960fa039c2ec0dc485e109758514f6763330a2cf.

--- Assets/_Game/00_Scripts/Utils/UI/MobileKeyboardTrigger.cs
+++ Assets/_Game/00_Scripts/Utils/UI/MobileKeyboardTrigger.cs
@@ -2,12 +2,10 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class MobileKeyboardTrigger : MonoBehaviour, ISelectHandler, IDeselectHandler
+public class MobileKeyboardTrigger : MonoBehaviour, ISelectHandler
 {
     [SerializeField]
     private TMP_InputField inputField;
-
-    private TouchScreenKeyboard _keyboard;
 
     private void Awake()
     {
@@ -15,44 +13,14 @@ public class MobileKeyboardTrigger : MonoBehaviour, ISelectHandler, IDeselectHan
             inputField = GetComponent<TMP_InputField>();
     }
 
-    private void Update()
-    {
-        if (_keyboard == null || !_keyboard.active)
-            return;
-
-        inputField.text = _keyboard.text;
-        inputField.caretPosition = _keyboard.text.Length;
-
-        if (_keyboard.status == TouchScreenKeyboard.Status.Done)
-        {
-            inputField.DeactivateInputField();
-            _keyboard = null;
-        }
-    }
-
     public void OnSelect(BaseEventData eventData)
     {
-        if (inputField == null)
-            return;
+        if (inputField != null)
+            inputField.ActivateInputField();
 
-        inputField.ActivateInputField();
-        _keyboard = TouchScreenKeyboard.Open(
-            inputField.text,
-            TouchScreenKeyboardType.Default,
-            false,
-            false,
-            false,
-            false,
-            "Name"
+        TouchScreenKeyboard.Open(
+            inputField != null ? inputField.text : "",
+            TouchScreenKeyboardType.Default
         );
-    }
-
-    public void OnDeselect(BaseEventData eventData)
-    {
-        if (_keyboard != null && _keyboard.active)
-        {
-            inputField.text = _keyboard.text;
-            _keyboard = null;
-        }
     }
 }
