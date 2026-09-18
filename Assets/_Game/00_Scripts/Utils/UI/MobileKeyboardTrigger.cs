47724d477161404507a183a381ebe71f0f36e574
Muhammad Zaini <163093916+muhammadzaini213@users.noreply.github.com>
2026-09-18 20:17:43 +0800

    fix: poll TouchScreenKeyboard text into TMP_InputField

--- Assets/_Game/00_Scripts/Utils/UI/MobileKeyboardTrigger.cs
+++ Assets/_Game/00_Scripts/Utils/UI/MobileKeyboardTrigger.cs
@@ -2,10 +2,12 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class MobileKeyboardTrigger : MonoBehaviour, ISelectHandler
+public class MobileKeyboardTrigger : MonoBehaviour, ISelectHandler, IDeselectHandler
 {
     [SerializeField]
     private TMP_InputField inputField;
+
+    private TouchScreenKeyboard _keyboard;
 
     private void Awake()
     {
@@ -13,14 +15,44 @@ public class MobileKeyboardTrigger : MonoBehaviour, ISelectHandler
             inputField = GetComponent<TMP_InputField>();
     }
 
+    private void Update()
+    {
+        if (_keyboard == null || !_keyboard.active)
+            return;
+
+        inputField.text = _keyboard.text;
+        inputField.caretPosition = _keyboard.text.Length;
+
+        if (_keyboard.status == TouchScreenKeyboard.Status.Done)
+        {
+            inputField.DeactivateInputField();
+            _keyboard = null;
+        }
+    }
+
     public void OnSelect(BaseEventData eventData)
     {
-        if (inputField != null)
-            inputField.ActivateInputField();
+        if (inputField == null)
+            return;
 
-        TouchScreenKeyboard.Open(
-            inputField != null ? inputField.text : "",
-            TouchScreenKeyboardType.Default
+        inputField.ActivateInputField();
+        _keyboard = TouchScreenKeyboard.Open(
+            inputField.text,
+            TouchScreenKeyboardType.Default,
+            false,
+            false,
+            false,
+            false,
+            "Name"
         );
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        if (_keyboard != null && _keyboard.active)
+        {
+            inputField.text = _keyboard.text;
+            _keyboard = null;
+        }
     }
 }
